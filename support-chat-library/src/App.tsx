0c351b882c7153d60c79286bf18e10ac71a1bd5b
Fernando Herrera <herrera.fernando1209@gmail.com>
2025-07-31 00:44:59 -0300

    start project support-chat-library

---
+++ support-chat-library/src/App.tsx
@@ -0,0 +1,17 @@
+import { Button } from "./components/ui/button"
+
+
+
+function App() {
+
+
+
+  return (
+    <>
+      <h1 className='text-3xl font-bold underline'>Hola Mundo</h1>
+      <Button>Enviar</Button>
+    </>
+  )
+}
+
+export default App
