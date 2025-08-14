0b74e8e6bfd727d112ca210bcfd1d53087f568fd
Fernando Herrera <herrera.fernando1209@gmail.com>
2025-08-14 20:55:23 -0300

    fin seccion 7

--- chat-support-framework/app/routes/auth/testing-args-page.tsx
+++ chat-support-framework/app/routes/auth/testing-args-page.tsx
@@ -1,12 +1,13 @@
 import { Link } from "react-router";
 import type { Route } from "./+types/testing-args-page";
+import { sleep } from "~/lib/sleep";
 
 export function meta() {
   return [
-    { title: "Very cool app" },
+    { title: "Support Chat" },
     {
       property: "og:title",
-      content: "Very cool app",
+      content: "Support Chat",
     },
     {
       name: "description",
@@ -15,12 +16,66 @@ export function meta() {
   ];
 }
 
+export function headers() {
+  return {
+    "X-Stretchy-Pants": "its for fun",
+    "Cache-Control": "max-age=300, s-maxage=3600",
+  };
+}
+
+export function links() {
+  return [
+    // {
+    //   rel: "icon",
+    //   href: "/favicon.png",
+    //   type: "image/png",
+    // },
+    // {
+    //   rel: "stylesheet",
+    //   href: "https://example.com/some/styles.css",
+    // },
+    // {
+    //   rel: "preload",
+    //   href: "/images/banner.jpg",
+    //   as: "image",
+    // },
+  ];
+}
+
+export async function loader({ params }: Route.LoaderArgs) {
+  console.log({ params, msg: "serverLoader" });
+
+  return { hola: "Mundo" };
+}
+
+export async function clientLoader({ params }: Route.ClientLoaderArgs) {
+  console.log({ params, msg: "CientLoader" });
+  await sleep(1500);
+  return { hola: "Nemesis", id: params.id };
+}
+
+export function HydrateFallback() {
+  return (
+    <div className="fixed inset-0 flex items-center justify-center bg-gradient-to-br from-blue-100 via-purple-100 to-pink-100">
+      <div className="flex flex-col items-center gap-4">
+        <div className="animate-spin rounded-full h-16 w-16 border-t-4 border-b-4 border-purple-500"></div>
+        <p className="text-xl font-semibold text-purple-700">Loading Game...</p>
+      </div>
+    </div>
+  );
+}
+
+clientLoader.hydrate = true as const;
+
 export default function TestingArgsPage({
   loaderData,
   actionData,
   params,
   matches,
 }: Route.ComponentProps) {
+  const { id } = params;
+  console.log(">>>>>>>>>>>>>>>>>", id);
+
   return (
     <div>
       <h1 className="text-3xl font-bold">Testing Args Page</h1>
--- chat-support-framework/app/routes/auth/testing-page.tsx
+++ chat-support-framework/app/routes/auth/testing-page.tsx
@@ -1,5 +1,30 @@
-import { Link } from "react-router";
+import { Form, NavLink, useNavigation } from "react-router";
 import type { Route } from "./+types/testing-page";
+import { sleep } from "~/lib/sleep";
+
+export async function action({ request }: Route.ActionArgs) {
+  await sleep(3000);
+  const data = await request.formData();
+  //data.get("title")
+
+  console.log("Server Action Data:");
+  console.log(data);
+  return { ok: true, message: "todo bien desde el serverAction" };
+}
+
+export async function clientAction({
+  serverAction,
+  request,
+}: Route.ClientActionArgs) {
+  await sleep(1500);
+
+  const formData = await request.clone().formData();
+  const allData = Object.fromEntries(formData);
+  // can still call the server action if needed
+  const data = await serverAction();
+  //return data;
+  return { message: "Hello from the client action! - Client", data, allData };
+}
 
 export async function loader() {
   return {
@@ -25,6 +50,11 @@ export default function MyRouteComponent({
   params,
   matches,
 }: Route.ComponentProps) {
+  const navigation = useNavigation();
+  const isPosting = navigation.state === "submitting";
+
+  console.log(isPosting);
+
   return (
     <div>
       <h1>Welcome to My Route with Props!</h1>
@@ -32,12 +62,37 @@ export default function MyRouteComponent({
       <p>Action Data: {JSON.stringify(actionData)}</p>
       <p>Route Parameters: {JSON.stringify(params)}</p>
       <p>Matched Routes: {JSON.stringify(matches)}</p>
-      <Link
-        to="/auth/testing-args"
-        className="text-blue-500 underline text-2xl"
+      <NavLink
+        to="/auth/testing-args/ABC-123"
+        className={({ isPending }) =>
+          isPending
+            ? "mt-3 text-red-500 underline text-2xl"
+            : "mt-3 text-blue-500 underline text-2xl"
+        }
       >
         Go to Testing Args Page
-      </Link>
+      </NavLink>
+      <Form className="mt-6 flex flex-col gap-4 w-80" method="post">
+        <input
+          type="text"
+          name="name"
+          placeholder="Nombre"
+          className="border rounded px-3 py-2 focus:outline-none focus:ring-2 focus:ring-blue-400"
+        />
+        <input
+          type="number"
+          name="age"
+          placeholder="Edad"
+          className="border rounded px-3 py-2 focus:outline-none focus:ring-2 focus:ring-blue-400"
+        />
+        <button
+          type="submit"
+          disabled={isPosting}
+          className="bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-600 transition disabled:opacity-50"
+        >
+          {isPosting ? "Submitting..." : "Submit"}
+        </button>
+      </Form>
     </div>
   );
 }
